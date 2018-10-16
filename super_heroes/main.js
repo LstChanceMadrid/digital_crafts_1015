352468528b0c4c149142da7de7e98d41ada29d40
LstChanceMadrid <michael.madrid@live.com>
2018-10-15 21:48:37 -0500

    updated code, added modals

--- super_heroes/main.js
+++ super_heroes/main.js
@@ -24,7 +24,7 @@ const requestMovies = () => {
                     <div class="movie-information">
                         <p onClick="requestMovieDescription('${movie.imdbID}')">More Details</p>
 
-                        <div id="${movie.imdbID}">
+                        <div class="modal" id="${movie.imdbID}">
                         </div>
                     </div>
 
@@ -51,50 +51,55 @@ const requestMovieDescription = (id) => {
     
     let movieModalContainer = document.getElementById(id);
 
-    
-    let request = new XMLHttpRequest();
+    let modal = document.getElementsByClassName('modal');
 
+    let request = new XMLHttpRequest();
+    
     request.onload = function() {
 
         let movieInfo = JSON.parse(this.responseText);
-        console.log(movieInfo.Poster)
-        
-
-        // let movieInfoPiece = movieInfo.Search.map(function(info) {
 
             let newMovie = `
                 <div class="movie-modal-container" style="background-image: url('${movieInfo.Poster}');">
-                <label class="movie-modal-title">${movieInfo.Title}</label>
-                Year
-                Rated
-                Released
-                Runtime
-                Genre
-                Director
-                Writer
-                Actors
-                Plot
-                Language
-                Country
-                Awards
-                Poster
-                Ratings
+
+                    <label class="movie-modal-title">${movieInfo.Title}</label>
+
+                    <button id="close" class="close">Close</button>
+
+                    <div class="movie-modal-information">${movieInfo.Rated}
+                    ${movieInfo.Year} ${movieInfo.Genre}<br />
+                    ${movieInfo.Released}, ${movieInfo.Runtime}
+                    </div>
+
+                    <div class="movie-modal-credits">- Writer: <small>${movieInfo.Writer}</small>
+                    <br />- Actors: <small>${movieInfo.Actors}</small>
+                    </div>
+
+                    <div class="movie-modal-plot">${movieInfo.Plot}</div>
+
+                    <div class="movie-modal-awards"><small>${movieInfo.Awards}</small></div>
                 </div>
-                `
-    // }
-        // );
+                `    
+                // <div class="movie-modal-rating">${movieInfo.Country}${movieInfo.Language}${movieInfo.Director}${movieInfo.Ratings}</div>
+                // </div>
+
         movieModalContainer.innerHTML = newMovie
         movieModalContainer.style.display = "flex";
+        
+        document.getElementById('close').addEventListener('click', function() {
+            this.parentElement.parentElement.removeChild(this.parentElement);
+        })
+
+
     }
     
+
+
     request.open('GET', `http://www.omdbapi.com/?i=${id}&apikey=${moviesKey}`);
     request.send();
 
     request;
 }
-
-
-
 
 
 
